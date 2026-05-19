507d9df8df508c998db65558656a1dd24df87a55
Ravi Kumar <ravibseb96@gmail.com>
2026-05-19 21:16:09 +0530

    Day-01 Completed

---
+++ Day-01/first.py
@@ -0,0 +1,65 @@
+name="Ravi Kumar"
+age=21
+print("My name is ",name,". My Age is ", age)
+print("My name is "+name) #only work for string concatenate
+
+# Data types of python
+price=22.909
+old=True
+character='c'
+a=None
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(character))
+print(type(a))
+
+a=20
+b=40
+print(a+b)
+
+# This is single line comment
+"""
+Multi 
+Line 
+Comment
+"""
+
+#Logical operator
+a=50
+b=20
+print(not False)
+print(not (a>b))
+
+val1=True
+val2=False
+print(val1 and val2)
+print(val1 or val2)
+
+#Type conversion:- automatic conversion(Inplicit conversion)
+c=2
+d=4.34
+sum=c+d
+print(sum)
+
+#Type casting :- casting manual
+e="8"
+f=4.34
+sum2=int(e)+f
+print(sum2)
+
+num=389
+num=str(num)
+print(type(num))
+
+
+#Input in python
+name2=input("Enter your name : ")
+print("Welcome ",name2)
+
+number1=int(input("Enter number1:"))
+number2=int(input("Enter number2:"))
+print(type(number1), number1+number2)
+
+
